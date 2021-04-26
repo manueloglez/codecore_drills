23a23fd1f6762a622ef17db7bf4729840c37cfed
manueloglez <mang.95@gmail.com>
2021-04-25 23:39:24 -0700

    minor fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
       def create
         @user = User.new user_params
         if @user.save
-          session[:user_id] = @user.id
-          redirect_to @user
+          # session[:user_id] = @user.id
+          flash[:warning] = "Thank you for registering! You will be notified when your account is approved."
+          redirect_to root_path
         else
           render :new
         end
